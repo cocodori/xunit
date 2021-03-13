07edaebd2364ec69af29748dd8af6c1ba3d01cf1
cocodori <moradulee@gmail.com>
2021-03-13 21:59:49 +0900

    setUp() 만들기

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -11,4 +11,11 @@ public class TestCaseTest extends TestCase{
         wasRun.run();
         Assert.assertEquals(true, wasRun.wasRun); //기대값, 실제값
     }
+
+    public void testSetUp() {
+        WasRun wasRun = new WasRun("testMethod");
+        Assert.assertEquals(false, wasRun.wasSetUp);
+        wasRun.run();
+        Assert.assertEquals(true, wasRun.wasSetUp);
+    }
 }
